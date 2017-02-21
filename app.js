84265b9fdc69229c1c5c14b7c4f4f5b21fbc71e5
sukitha <sukitha@duosoftware.com>
2017-02-21 16:38:29 +0530

    mongo cluster support added

--- app.js
+++ app.js
@@ -41,24 +41,91 @@ var Login = require("./Login");
 var tenantService=require("./TenantService");
 
 
+//var mongoip=config.Mongo.ip;
+//var mongoport=config.Mongo.port;
+//var mongodb=config.Mongo.dbname;
+//var mongouser=config.Mongo.user;
+//var mongopass = config.Mongo.password;
+//var mongoose = require('mongoose');
+//var connectionstring = util.format('mongodb://%s:%s@%s:%d/%s',mongouser,mongopass,mongoip,mongoport,mongodb)
+//mongoose.connect(connectionstring);
+//
+//mongoose.connection.once('open', function() {
+//    console.log("Connected to db");
+//});
+
+
+var util = require('util');
 var mongoip=config.Mongo.ip;
 var mongoport=config.Mongo.port;
 var mongodb=config.Mongo.dbname;
 var mongouser=config.Mongo.user;
 var mongopass = config.Mongo.password;
+var mongoreplicaset= config.Mongo.replicaset;
+
+var mongoose = require('mongoose');
+var connectionstring = '';
+if(util.isArray(mongoip)){
+
+    mongoip.forEach(function(item){
+        connectionstring += util.format('%s:%d,',item,mongoport)
+    });
+
+    connectionstring = connectionstring.substring(0, connectionstring.length - 1);
+    connectionstring = util.format('mongodb://%s:%s@%s/%s',mongouser,mongopass,connectionstring,mongodb);
+
+    if(mongoreplicaset){
+        connectionstring = util.format('%s?replicaSet=%s',connectionstring,mongoreplicaset) ;
+    }
+}else{
+
+    connectionstring = util.format('mongodb://%s:%s@%s:%d/%s',mongouser,mongopass,mongoip,mongoport,mongodb)
+}
+
+
+mongoose.connect(connectionstring,{server:{auto_reconnect:true}});
+
+
+mongoose.connection.on('error', function (err) {
+    console.error( new Error(err));
+    mongoose.disconnect();
+
+});
+
+mongoose.connection.on('opening', function() {
+    console.log("reconnecting... %d", mongoose.connection.readyState);
+});
+
+
+mongoose.connection.on('disconnected', function() {
+    console.error( new Error('Could not connect to database'));
+    mongoose.connect(connectionstring,{server:{auto_reconnect:true}});
+});
+
+mongoose.connection.once('open', function() {
+    console.log("Connected to db");
+
+});
+
+
+mongoose.connection.on('reconnected', function () {
+    console.log('MongoDB reconnected!');
+});
+
+
+
+process.on('SIGINT', function() {
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    });
+});
 
 
 var port = config.Host.port || 3000;
 var host = config.Host.vdomain || 'localhost';
 
 
-var mongoose = require('mongoose');
-var connectionstring = util.format('mongodb://%s:%s@%s:%d/%s',mongouser,mongopass,mongoip,mongoport,mongodb)
-mongoose.connect(connectionstring);
-
-mongoose.connection.once('open', function() {
-    console.log("Connected to db");
-});
 
 app.set('view engine', 'ejs');
 
