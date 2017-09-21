b69d9fa8ad010c51008877f1a89908104f26d235
heshanindika <k.heshan.indika@gmail.com>
2017-09-21 17:45:20 +0530

    fix filter navigation types

--- NavigationService.js
+++ NavigationService.js
@@ -11,8 +11,8 @@ var Org = require('dvp-mongomodels/model/Organisation');
 
 function UniqueObjectArray(array, field) {
     var processed = [];
-    for (var i=array.length-1; i>=0; i--) {
-        if (processed.indexOf(array[i][field])<0) {
+    for (var i = array.length - 1; i >= 0; i--) {
+        if (processed.indexOf(array[i][field]) < 0) {
             processed.push(array[i][field]);
         } else {
             array.splice(i, 1);
@@ -21,26 +21,26 @@ function UniqueObjectArray(array, field) {
     return array;
 }
 
-function GetResources(resources){
+function GetResources(resources) {
     var e = new EventEmitter();
     process.nextTick(function () {
         if (Array.isArray(resources) && resources.length > 0) {
             var count = 0;
             for (var i = 0; i < resources.length; i++) {
                 var resource = resources[i];
-                Resource.findOne({resourceName: resource.resourceName}, function(err, resource) {
+                Resource.findOne({resourceName: resource.resourceName}, function (err, resource) {
                     count++;
                     if (err) {
                         console.log(err);
-                    }else{
-                        e.emit('validateResource',resource);
+                    } else {
+                        e.emit('validateResource', resource);
                     }
-                    if(count == resources.length){
+                    if (count == resources.length) {
                         e.emit('endValidateResources');
                     }
                 });
             }
-        }else {
+        } else {
             e.emit('endValidateResources');
         }
     });
@@ -48,21 +48,21 @@ function GetResources(resources){
     return (e);
 }
 
-function GetAllConsoles(req, res){
+function GetAllConsoles(req, res) {
     logger.debug("DVP-UserService.GetAllConsoles Internal method ");
 
     var jsonString;
-    Console.find({}, function(err, allConsole) {
+    Console.find({}, function (err, allConsole) {
         if (err) {
             jsonString = messageFormatter.FormatMessage(err, "Get All Navigation Failed", false, undefined);
-        }else{
-            var newConsoles =[];
-            if(allConsole) {
+        } else {
+            var newConsoles = [];
+            if (allConsole) {
                 for (var a = 0; a < allConsole.length; a++) {
 
                     var console1 = allConsole[a];
                     var newResult = {consoleName: console1.consoleName, consoleNavigation: []};
-                    if(console1.consoleNavigation) {
+                    if (console1.consoleNavigation) {
                         for (var i = 0; i < console1.consoleNavigation.length; i++) {
                             var navigation = console1.consoleNavigation[i];
                             var newNavigation = {
@@ -70,7 +70,7 @@ function GetAllConsoles(req, res){
                                 navigationStatus: navigation.navigationStatus
                             };
                             var newResourceScopes = [];
-                            if(navigation.resources) {
+                            if (navigation.resources) {
                                 for (var j = 0; j < navigation.resources.length; j++) {
                                     var resource = navigation.resources[j];
                                     for (var k in resource.scopes) {
@@ -92,7 +92,7 @@ function GetAllConsoles(req, res){
     });
 }
 
-function GetAllConsolesByUserRole(req, res){
+function GetAllConsolesByUserRole(req, res) {
     logger.debug("DVP-UserService.GetAllConsolesByUserRole Internal method ");
 
     var jsonString;
@@ -100,7 +100,10 @@ function GetAllConsolesByUserRole(req, res){
     var tenant = parseInt(req.user.tenant);
 
 
-    Org.findOne({tenant: tenant, id: company}).populate({path: 'packageDetails.veeryPackage',populate : {path: 'Package'}}).exec( function (err, org) {
+    Org.findOne({tenant: tenant, id: company}).populate({
+        path: 'packageDetails.veeryPackage',
+        populate: {path: 'Package'}
+    }).exec(function (err, org) {
 
         if (err) {
 
@@ -114,48 +117,58 @@ function GetAllConsolesByUserRole(req, res){
                 if (org.packageDetails.length > 0) {
                     var availableNavigationTypes = [];
                     org.packageDetails.forEach(function (pkg) {
-                        if(availableNavigationTypes.indexOf(pkg.veeryPackage.navigationType) === -1) {
+                        if (availableNavigationTypes.indexOf(pkg.veeryPackage.navigationType) === -1) {
                             availableNavigationTypes.push(pkg.veeryPackage.navigationType);
                         }
                     });
 
-                    Console.find({consoleUserRoles:req.params.roleType, "consoleNavigation.navigationTypes": {$in:availableNavigationTypes}}, function(err, allConsole) {
-                    if (err) {
-                        jsonString = messageFormatter.FormatMessage(err, "Get All Navigation Failed", false, undefined);
-                    }else{
-                        var newConsoles =[];
-                        if(allConsole) {
-                            for (var a = 0; a < allConsole.length; a++) {
-                                var console1 = allConsole[a];
-                                var newResult = {consoleName: console1.consoleName, consoleNavigation: []};
-                                if(console1.consoleNavigation) {
-                                    for (var i = 0; i < console1.consoleNavigation.length; i++) {
-                                        var navigation = console1.consoleNavigation[i];
-                                        var newNavigation = {
-                                            navigationName: navigation.navigationName,
-                                            navigationStatus: navigation.navigationStatus
-                                        };
-                                        var newResourceScopes = [];
-                                        if(navigation.resources) {
-                                            for (var j = 0; j < navigation.resources.length; j++) {
-                                                var resource = navigation.resources[j];
-                                                for (var k = 0; k < resource.scopes.length; k++) {
-                                                    var scope = resource.scopes[k];
-                                                    newResourceScopes.push(scope);
+                    Console.find({consoleUserRoles: req.params.roleType}, function (err, allConsole) {
+                        if (err) {
+                            jsonString = messageFormatter.FormatMessage(err, "Get All Navigation Failed", false, undefined);
+                        } else {
+                            var newConsoles = [];
+                            if (allConsole) {
+                                for (var a = 0; a < allConsole.length; a++) {
+                                    var console1 = allConsole[a];
+                                    var newResult = {consoleName: console1.consoleName, consoleNavigation: []};
+                                    if (console1.consoleNavigation) {
+
+                                        console1.consoleNavigation.forEach(function (navigation) {
+                                            if (availableNavigationTypes.every(function (u, i) {
+                                                    return u === navigation.navigationTypes[i];
+                                                })
+                                            ) {
+                                                var newNavigation = {
+                                                    navigationName: navigation.navigationName,
+                                                    navigationStatus: navigation.navigationStatus
+                                                };
+                                                var newResourceScopes = [];
+                                                if (navigation.resources) {
+                                                    for (var j = 0; j < navigation.resources.length; j++) {
+                                                        var resource = navigation.resources[j];
+                                                        for (var k = 0; k < resource.scopes.length; k++) {
+                                                            var scope = resource.scopes[k];
+                                                            newResourceScopes.push(scope);
+                                                        }
+                                                    }
+                                                    newNavigation.resources = newResourceScopes;//UniqueObjectArray(newResourceScopes,"scopes");
                                                 }
+                                                newResult.consoleNavigation.push(newNavigation);
+                                            } else {
+                                                console.log(false);
                                             }
-                                            newNavigation.resources = newResourceScopes;//UniqueObjectArray(newResourceScopes,"scopes");
-                                        }
-                                        newResult.consoleNavigation.push(newNavigation);
+
+
+                                        });
+
+                                        newConsoles.push(newResult);
                                     }
-                                    newConsoles.push(newResult);
                                 }
                             }
+                            jsonString = messageFormatter.FormatMessage(err, "Get All Navigation Successful", true, newConsoles);
                         }
-                        jsonString = messageFormatter.FormatMessage(err, "Get All Navigation Successful", true, newConsoles);
-                    }
-                    res.end(jsonString);
-                });
+                        res.end(jsonString);
+                    });
 
                 } else {
 
@@ -176,41 +189,18 @@ function GetAllConsolesByUserRole(req, res){
     });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
-function GetConsole(req, res){
+function GetConsole(req, res) {
     logger.debug("DVP-UserService.GetConsole Internal method ");
     var jsonString;
-    Console.findOne({consoleName: req.params.consoleName}, function(err, console) {
+    Console.findOne({consoleName: req.params.consoleName}, function (err, console) {
         if (err) {
             jsonString = messageFormatter.FormatMessage(err, "Get Console Failed", false, undefined);
-        }else{
-            var newResult = {consoleName: console.consoleName, consoleNavigation:[]};
+        } else {
+            var newResult = {consoleName: console.consoleName, consoleNavigation: []};
 
-            if(console && console.consoleNavigation) {
+            if (console && console.consoleNavigation) {
                 for (var i = 0; i < console.consoleNavigation.length; i++) {
                     var navigation = console.consoleNavigation[i];
                     var newNavigation = {
@@ -218,7 +208,7 @@ function GetConsole(req, res){
                         navigationStatus: navigation.navigationStatus
                     };
                     var newResourceScopes = [];
-                    for (var j = 0; j< navigation.resources.length; j++) {
+                    for (var j = 0; j < navigation.resources.length; j++) {
                         var resource = navigation.resources[j];
                         for (var k = 0; k < resource.scopes.length; k++) {
                             var scope = resource.scopes[k];
@@ -235,7 +225,7 @@ function GetConsole(req, res){
     });
 }
 
-function DeleteConsole(req,res){
+function DeleteConsole(req, res) {
     logger.debug("DVP-UserService.DeleteConsole Internal method ");
 
     var jsonString;
@@ -261,7 +251,7 @@ function DeleteConsole(req,res){
     });
 }
 
-function CreateConsole(req, res){
+function CreateConsole(req, res) {
     logger.debug("DVP-UserService.CreateConsole Internal method ");
     var jsonString;
 
@@ -284,23 +274,23 @@ function CreateConsole(req, res){
     });
 }
 
-function UpdateConsole(req, res){
+function UpdateConsole(req, res) {
     logger.debug("DVP-UserService.UpdateConsole Internal method ");
 
     var jsonString;
 
     req.body.updated_at = Date.now();
-    Console.findOneAndUpdate({consoleName: req.params.consoleName}, req.body, function(err, console) {
+    Console.findOneAndUpdate({consoleName: req.params.consoleName}, req.body, function (err, console) {
         if (err) {
             jsonString = messageFormatter.FormatMessage(err, "Update Console Failed", false, undefined);
-        }else{
+        } else {
             jsonString = messageFormatter.FormatMessage(err, "Update Console Successful", true, console);
         }
         res.end(jsonString);
     });
 }
 
-function AddNavigationToConsole(req, res){
+function AddNavigationToConsole(req, res) {
     logger.debug("DVP-UserService.AddNavigationToConsole Internal method ");
     var jsonString;
 
@@ -314,8 +304,8 @@ function AddNavigationToConsole(req, res){
     });
 
     var gr = GetResources(req.body.resources);
-    gr.on('validateResource',function(resource){
-        if(resource) {
+    gr.on('validateResource', function (resource) {
+        if (resource) {
             for (var i in req.body.resources) {
                 var bResource = req.body.resources[i];
                 if (bResource && bResource.resourceName == resource.resourceName) {
@@ -323,19 +313,19 @@ function AddNavigationToConsole(req, res){
                     break;
                 }
             }
-        }else{
+        } else {
             logger.warn('No Resource Service found');
         }
     });
-    gr.on('endValidateResources',function(){
-        Console.findOne({consoleName: req.params.consoleName}, function(err, console) {
+    gr.on('endValidateResources', function () {
+        Console.findOne({consoleName: req.params.consoleName}, function (err, console) {
             if (err) {
                 jsonString = messageFormatter.FormatMessage(err, "Get Console Failed", false, undefined);
                 res.end(jsonString);
-            }else{
-                if(console) {
-                    Console.findOne({$and:[{"consoleNavigation.navigationName":navigation.navigationName},{"consoleName":req.params.consoleName}]}, function(err, isExists) {
-                        if(isExists){
+            } else {
+                if (console) {
+                    Console.findOne({$and: [{"consoleNavigation.navigationName": navigation.navigationName}, {"consoleName": req.params.consoleName}]}, function (err, isExists) {
+                        if (isExists) {
                             console.updated_at = Date.now();
                             for (var i = 0; i < console.consoleNavigation.length; i++) {
                                 if (console.consoleNavigation[i].navigationName.search(navigation.navigationName) != -1) {
@@ -343,7 +333,7 @@ function AddNavigationToConsole(req, res){
                                     break;
                                 }
                             }
-                        }else{
+                        } else {
                             console.updated_at = Date.now();
                             console.consoleNavigation.push(navigation);
                         }
@@ -356,7 +346,7 @@ function AddNavigationToConsole(req, res){
                             res.end(jsonString);
                         });
                     });
-                }else{
+                } else {
                     jsonString = messageFormatter.FormatMessage(err, "No Console Found", false, undefined);
                     res.end(jsonString);
                 }
@@ -365,17 +355,17 @@ function AddNavigationToConsole(req, res){
     });
 }
 
-function RemoveNavigationFromConsole(req,res){
+function RemoveNavigationFromConsole(req, res) {
     logger.debug("DVP-UserService.RemoveNavigationToConsole Internal method ");
 
     var jsonString;
 
-    Console.findOne({consoleName: req.params.consoleName}, function(err, console) {
+    Console.findOne({consoleName: req.params.consoleName}, function (err, console) {
         if (err) {
             jsonString = messageFormatter.FormatMessage(err, "Find Console Failed", false, undefined);
             res.end(jsonString);
-        }else{
-            if(console) {
+        } else {
+            if (console) {
                 console.updated_at = Date.now();
                 for (var i = 0; i < console.consoleNavigation.length; i++) {
                     if (console.consoleNavigation[i].navigationName.search(req.params.navigationName) != -1) {
@@ -392,7 +382,7 @@ function RemoveNavigationFromConsole(req,res){
                     }
                     res.end(jsonString);
                 });
-            }else{
+            } else {
                 jsonString = messageFormatter.FormatMessage(err, "No Console Found", false, undefined);
                 res.end(jsonString);
             }
